f1194f10f3a8978ec8e10dd0469610d8ac31a526
yoda <ericryhan@gmail.com>
2023-10-14 19:30:06 -0700

    added some shit to the first page
    still blank and white and nothing but progress

--- app/index.js
+++ app/index.js
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+import { Text, View, ScrollView, SafeAreaView } from 'react-native';
+import {Stack, useRouter} from 'expo-router';
 
-import { View, Text } from 'react-native'; 
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home
-            </Text>
-        </View>
+        <SafeAreaView style = {{ flex: 1, backgroundColor: COLORS.lightwhite}}>
+            <Stack.Screen
+              options={{
+                headerStyle: {backgroundColor: COLORS.lightWhite}, headerShadowVisible: false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl = {icons.menu} dimension = "60%" />
+                ),
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl = {icons.profile} dimension = "100%" />
+                ),
+                headerTitle: ""
+            }}
+        />
+        </SafeAreaView>
     )
 }
 
