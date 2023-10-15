e1d9191d1bb688a1777b1c475248db07f8760369
heonjwa <heonjwa@gmail.com>
2023-10-14 21:47:41 -0700

    resolved

--- app/index.js
+++ app/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; // Import createNativeStackNavigator
 import Screen1 from '../components/Screen1'; // Import your screen components
@@ -19,21 +18,20 @@ const Home = () => {
   );
 };
 
-    const router = useRouter();
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="Saved Translations" component={Screen1} 
-            options = {{
-                /*
-                print list of past translations, refer to 
-                render in editor.tsx
-                */
-                
-            }}
-            />
-            <Tab.Screen name="Camera" component={CameraScreen} />
-        </Tab.Navigator>
-    );
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Translations"
+        component={Screen1}
+        options={{
+          // Customize options for "Saved Translations" tab
+        }}
+      />
+      <Tab.Screen name="Camera" component={CameraScreen} />
+    </Tab.Navigator>
+  );
 }
 
 export default Home;
+
