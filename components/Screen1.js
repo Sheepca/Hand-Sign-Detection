73939fee03e03ec33c6543de71018ded71c0f080
yoda <ericryhan@gmail.com>
2023-10-15 04:17:33 -0700

    not fucked, two boxes

--- components/Screen1.js
+++ components/Screen1.js
@@ -1,119 +1,119 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, activeTab,fontLoaded,ActivityIndicator } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, ActivityIndicator } from 'react-native';
 
+function Screen1({ navigation }) {
+  const [fontLoaded, setFontLoaded] = useState(true); // You should set the actual font loading state
+  const [activeTab, setActiveTab] = useState('Contacts'); // Define and set the active tab state
+  const [groupedContacts, setGroupedContacts] = useState({}); // Define the grouped contacts state
 
+  const text = "ooga booga";
+  const table = [];
 
-function Screen1() {
-    let text  = "ooga booga"; 
-    let table = [];
-    useEffect(() => {
-        sendText(text);
-        buildTable();
-      }, []);
-    return (
-        <View style={translate.container}>
-            <View style={translate.textBoxContainer}>
-                 <Text style={translate.textBox}>Translation: </Text>
-            </View>
-            <View style = {styles.container}>
-            <View style={styles.container}>
-                <View style={dividers.tabs}>
-                    <TouchableOpacity
-                        style={[styles.tabButton, activeTab === 'Contacts' ? dividers.container : null]}
-                        onPress={() => navigation.navigate('Contacts')}
-                    >
-                        <Text style={styles.tabButtonText}>previous translations: </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={[styles.tabButton, activeTab === 'Dial' ? styles.activeTab : null]}
-                        onPress={() => navigation.navigate('Dial')} // Navigate to 'Dial' when 'Dial' tab is clicked
-                    >
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.contactsContainer}>
-                    {fontLoaded ? (
-                        <ScrollView>
-                            {activeTab === 'Contacts' ? (
-                                <View>
-                                    <Text style={[styles.groupTitle, { color: '#5267B3', textAlign: '' }]}>Contacts</Text>
-                                    {Object.keys(groupedContacts)
-                                        .sort()
-                                        .map((letter) => (
-                                            <View key={letter}>
-                                                <Text style={[styles.groupTitle, { color: '#5267B3', textAlign: 'left' }]}>{letter}</Text>
-                                                {groupedContacts[letter].map((contact) => renderContactItem(contact))}
-                                            </View>
-                                        ))}
-                                </View>
-                            ) : null}
-                        </ScrollView>
-                    ) : (
-                        <ActivityIndicator size="large" color="#9E896A" />
-                    )}
-                </View>
-            </View>
-            </View>
+  useEffect(() => {
+    // Simulated data (you can replace this with actual data fetching)
+    const groupedContactsData = {
+      A: ['Alice', 'Amy'],
+      B: ['Bob'],
+    };
+
+    setGroupedContacts(groupedContactsData);
+    // Build table here if needed
+  }, []);
+
+  return (
+    <View style={translate.container}>
+      <View style={translate.textBoxContainer}>
+        <Text style={translate.textBox}>Translation: {text}</Text>
+      </View>
+
+      <View style={styles.container}>
+        <View style={dividers.container}>
+          <Text
+            // style={[styles.tabButton, activeTab === 'Contacts' ? dividers.container : null]}
+            // onPress={() => setActiveTab('Contacts')}
+          >
+            <Text style={styles.prevBoxContainer}>previous translations: </Text>
+          </Text>
+          <Text
+
+            // onPress={() => navigation.navigate('Dial')}
+          >
+          </Text>
         </View>
-    );
+        <View style={styles.contactsContainer}>
+            {/* <ActivityIndicator size="large" color="#9E896A" /> */}
+        </View> 
+      </View>
+    </View>
+  );
 }
 
 const translate = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#E6C8FF', // Background color of the screen
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
-        padding: 30,
-      },
-      textBoxContainer: {
-        width: '100%', // Adjust the width as needed
-        backgroundColor: '#ededed', // Background color of the box
-        padding: 15,
-        borderWidth: 1,
-        borderColor: 'blue', // Border color
-        borderRadius: 5, // Border radius for rounded corners
-      },
-      textBox: {
-        fontSize: 16,
-      },
+  container: {
+    flex: 1,
+    backgroundColor: '#E6C8FF',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    padding: 30,
+  },
+  textBoxContainer: {
+    width: '100%',
+    backgroundColor: '#ededed',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 5,
+  },
+  textBox: {
+    fontSize: 16,
+  },
 });
 
 const dividers = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#E6C8FF',
-        borderWdith: 1,
-        borderColor: 'blue',
-        borderRadius: 5,
-    }
-})
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: 20,
-        justifyContent: 'top',
-        alignItems: 'top',
-        // backgroundColor: '#f0f0f0',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#E6C8FF',
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 5,
+  },
 });
 
-sendText = (text) => {
-    fetch("/api/saveTranslation", {method: "POST", body: text, headers: {'Content-Type': "application/json"}})
-}
-
-buildTable = () => {
-    fetch("/api/listSaved", {method: "POST", headers: {'Content-Type': "application/json"}})
-    .then(data => {
-        list = [];
-        for (item in data) {
-            list.push(
-                <View>
-                    <Text>{item}</Text>
-                </View>
-            )
-        }   
-        table = list;
-    });
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+    justifyContent: 'top', // Should be 'flex-start'
+    alignItems: 'top', // Should be 'flex-start'
+  },
+  textBoxContainer: {
+    width: '100%',
+    backgroundColor: '#ededed',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 5,
+    fontSize: 16,
+  },
+  prevBoxContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  tabButton: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    margin: 5,
+  },
+  contactsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  groupTitle: {
+    color: '#5267B3',
+    textAlign: 'left',
+  },
+});
 
 export default Screen1;
